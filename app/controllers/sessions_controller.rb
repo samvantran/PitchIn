b1541e949139ed2c0601452fa5cf5d27c7ed3dc3
draisy <draisy@gmail.com>
2014-11-25 17:04:49 -0500

    premerge show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,7 @@ class SessionsController < ApplicationController
   if session[:is_organization] == false
     user = Volunteer.find_or_create_from_auth_hash(self.auth_hash)
     login(user, "volunteer")
-    if user.opportunity != nil
       redirect_to volunteer_path(user)    
-    else
-      redirect_to edit_volunteer_path(user)
-    end
   else
     user = Organization.find_or_create_from_auth_hash(self.auth_hash)
     login(user, "organization")
