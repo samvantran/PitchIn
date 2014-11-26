e0e4f1ec67eeb4debfa52f91e42f5f07e91dc172
draisy <draisy@gmail.com>
2014-11-26 16:44:51 -0500

    fix bug

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -37,8 +37,8 @@ class VolunteersController < ApplicationController
   end
 
   def show
-    #@volunteer = Volunteer.find(session[:volunteer_id])
-    @volunteer = Volunteer.find(params[:id])
+    @volunteer = Volunteer.find(session[:volunteer_id])
+    #@volunteer = Volunteer.find(params[:id])
   end
 
   def final
